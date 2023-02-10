a546e81c0856cb6f5a80ac96c9b7a5e5c1884c13
Cinnayanny <jenniferwood2005@gmail.com>
2023-02-10 13:39:23 +1100

    Init index.php and template.php

---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+function footer():string
+{
+    date_default_timezone_set('Australia/Canberra');
+    $filename = basename($_SERVER["SCRIPT_FILENAME"]);
+    $footer = "This page was last modified: " . date("F d Y H:i:s.", filemtime($filename));
+    return $footer;
+}
+
+function sanitiseData($unsanitisedData)
+{
+    $unsanitisedData = trim($unsanitisedData);
+    $unsanitisedData = stripslashes($unsanitisedData);
+    $sanitisedData = htmlspecialchars($unsanitisedData);
+    return $sanitisedData;
+}
+
+?>
