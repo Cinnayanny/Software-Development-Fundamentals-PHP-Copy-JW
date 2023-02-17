aff93945f79f1de3674f3a4b88d14a1b2d369e84
Cinnayanny <jenniferwood2005@gmail.com>
2023-02-17 14:25:28 +1100

    comment contact.php

--- contact.php
+++ contact.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($formError == false) {
         $emailAddress = $_POST['inputEmail'];
         $messageSubmitted = $_POST['inputMessage'];
-        # Oddly, you can't put sanitiseData on these because they'll not show up
+        # Oddly, you can't put sanitiseData on these because the message won't show up
 
         echo $emailAddress;
         echo "<p>";
